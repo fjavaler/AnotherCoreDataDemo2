914d59820a7cadac90ef9e1c1c68426c69478013
Frederick Javalera <fjavalera@goldpointsystems.com>
2021-09-15 14:32:32 -0600

    Initial Commit

---
+++ AnotherCoreDataDemo2/AnotherCoreDataDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  AnotherCoreDataDemo2App.swift
+//  AnotherCoreDataDemo2
+//
+//  Created by Frederick Javalera on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AnotherCoreDataDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
